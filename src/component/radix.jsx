24ea8b59c99c7dd6e483fea905e56d9b6dc4d2de
Your Name <your_email@example.com>
2024-06-13 20:31:47 +0500

    bg

--- src/component/radix.jsx
+++ src/component/radix.jsx
@@ -9,47 +9,47 @@ const radix = () => (
   >
     <Tabs.List className="shrink-0 flex border-b border-mauve6" aria-label="Manage your account">
       <Tabs.Trigger
-        className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+        className=" bg-[#252836] px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-[#FFFFFF] select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-[#FFFFFF] data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
         value="tab1"
       >
-        Account
+        Hot Dishes
       </Tabs.Trigger>
       <Tabs.Trigger
-        className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+        className="bg-[#252836] px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-[#FFFFFF] select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-[#FFFFFF] data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
         value="tab2"
       >
         Password
       </Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content
-      className="grow p-5 bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
+      className="grow p-5 bg-[#252836] rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
       value="tab1"
     >
-      <p className="mb-5 text-mauve11 text-[15px] leading-normal">
+      <p className="mb-5 text-[#FFFFFF] text-[15px] leading-normal">
         Make changes to your account here. Click save when you're done.
       </p>
       <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-        <label className="text-[13px] leading-none mb-2.5 text-violet12 block" htmlFor="name">
+        <label className="text-[13px] leading-none mb-2.5 text-[#FFFFFF] block" htmlFor="name">
           Name
         </label>
         <input
-          className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+          className="grow shrink-0 bg-[#2D303E] rounded px-2.5 text-[15px] text-[#FFFFFF] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
           id="name"
-          defaultValue="Pedro Duarte"
+          defaultValue="Pedro Duarte "
         />
       </fieldset>
       <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-        <label className="text-[13px] leading-none mb-2.5 text-violet12 block" htmlFor="username">
+        <label className="text-[13px] leading-none mb-2.5 text-[#FFFFFF] block" htmlFor="username">
           Username
         </label>
         <input
-          className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+          className="grow shrink-0  bg-[#2D303E] text-[#FFFFFF] rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
           id="username"
           defaultValue="@peduarte"
         />
       </fieldset>
       <div className="flex justify-end mt-5">
-        <button className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-green4 text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default">
+        <button className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-[#252836] text-[#FFFFFF] hover:bg-[#EA7C69] focus:shadow-[0_0_0_2px] focus:shadow-green7 border border-[#FFFFFF] cursor-default">
           Save changes
         </button>
       </div>
