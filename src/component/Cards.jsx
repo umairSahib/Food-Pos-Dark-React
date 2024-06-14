ef81744d3d530eeed7aadce15f3fb67d4f0dbf9c
Your Name <your_email@example.com>
2024-06-14 21:47:17 +0500

    ns

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -3,33 +3,82 @@ import React from 'react'
 const Cards = () => {
   return (
     <div>
-  <div className="bg-[#1F1D2B]  rounded-lg border-1 border-[#393C49] w-[221px] h-[299px]" >
-     <div className="flex justify-center mt-6">
-        <img src="images/image(1).png" alt="image" />
-         
-     </div>
-     <div className="mt-[14px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned </div>
-     <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm"> seafood noodles </div>
-     <div className="mt-[8px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm"> $ 2.29 20 Bowls </div>
+ 
 
-     <div className="flex justify-center items-center mt-[30px] bg-[#50343a] rounded-bl-[8px] rounded-br-[8px] rounded-tl-0 rounded-tr-0 w-[221px] h-[52px]">
-        <i className="fa-solid fa-pen flex justify-center items-center text-[#EA7C69]"></i>
-        <div className="font-barlow font-semibold text-sm text-[#EA7C69]">Edit dish</div>
-      </div>
-      
-    </div>
 
-    <div className="mt-[45px] bg-[#1F1D2B] rounded-lg border-1 border-#393C49 w-[196px] h-[226px] flex flex-col items-center">
-      <div className="w-[196px] h-[196px] flex justify-center items-center -mt-[40px]">
-        <img src="images/image(1).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
-      </div>
+    <div className="mt-[64px] flex gap-[18px]">
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/image(1).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">spinach leaf</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
+              </div>
+
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/images(4).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Hot spicy fried rice  </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">with omelet</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.49</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">13 Bowls available</div>
+              </div>
     
-      <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned</div>
-      <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">seafood noodles</div>
-      <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 2.29</div>
-      <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">20 Bowls available</div>
-    </div>
-    </div>
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/image(6).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy instant noodle </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">with special omelette</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.59</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">17 Bowls available</div>
+              </div>
+              
+              
+        </div>
+    <div className="mt-[64px] flex gap-[18px]">
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/images(4).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">spinach leaf</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
+              </div>
+
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/images(4).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Hot spicy fried rice  </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">with omelet</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.49</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">13 Bowls available</div>
+              </div>
+    
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
+                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/image(6).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy instant noodle </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">with special omelette</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.59</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">17 Bowls available</div>
+              </div>
+              
+              
+        </div>
+  </div>
   )
 }
 
--- src/component/Tabs.jsx
+++ src/component/Tabs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
+import Cards from '../component/Cards'
 
 
 const tabs = () => (
@@ -49,34 +50,10 @@ const tabs = () => (
     className="grow p-5 bg-[#252836] rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
     value="tab1"
   >
-    <p className="mb-5 text-[#FFFFFF] text-[15px] leading-normal">
-      Make changes to your account here. Click save when you're done.
-    </p>
-    <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-      <label className="text-[13px] leading-none mb-2.5 text-[#FFFFFF] block" htmlFor="name">
-        Name
-      </label>
-      <input
-        className="grow shrink-0 bg-[#2D303E] rounded px-2.5 text-[15px] text-[#FFFFFF] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-        id="name"
-        defaultValue="Pedro Duarte "
-      />
-    </fieldset>
-    <fieldset className="mb-[15px] w-full flex flex-col justify-start">
-      <label className="text-[13px] leading-none mb-2.5 text-[#FFFFFF] block" htmlFor="username">
-        Username
-      </label>
-      <input
-        className="grow shrink-0  bg-[#2D303E] text-[#FFFFFF] rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-        id="username"
-        defaultValue="@peduarte"
-      />
-    </fieldset>
-    <div className="flex justify-end mt-5">
-      <button className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-[#252836] text-[#FFFFFF] hover:bg-[#EA7C69] focus:shadow-[0_0_0_2px] focus:shadow-green7 border border-[#FFFFFF] cursor-default">
-        Save changes
-      </button>
-    </div>
+   <Cards/>
+   
+   
+    
   </Tabs.Content>
   <Tabs.Content
     className="grow p-5 bg-[#252836] rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
