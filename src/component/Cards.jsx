9954a7c71e3963e66756ae421cf6dc1830ada8d7
Your Name <your_email@example.com>
2024-06-20 13:17:51 +0500

    hotdish

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -5,9 +5,39 @@ const Cards = () => {
     <div>
  
  <div className="mt-[64px] flex gap-[18px]">
-            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/image(1).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish1.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned</div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">seafood noodles</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 2.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">20 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish2.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned</div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">seafood noodles</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 2.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">20 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish3.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned</div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">seafood noodles</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 2.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">20 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish4.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy seasoned</div>
@@ -16,9 +46,9 @@ const Cards = () => {
                 <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">20 Bowls available</div>
               </div>
 
-              <div className=" bg-[#1F1D2B] rounded-[18px] w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/image(2).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+              <div className=" bg-[#1F1D2B] rounded-[18px] w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish5.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Salted Pasta with </div>
@@ -27,9 +57,9 @@ const Cards = () => {
                 <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">11 Bowls available</div>
               </div>
     
-              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/images.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish6.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Beef dumpling in hot </div>
@@ -41,9 +71,39 @@ const Cards = () => {
               
         </div>
         <div className="mt-[64px] flex gap-[18px]">
-            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/images(4).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish7.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">spinach leaf</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish8.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">spinach leaf</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish9.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+                </div>
+              
+                <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
+                <div className="mt-[0px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">spinach leaf</div>
+                <div className="mt-[8px] flex justify-center text-[#FFFFFF] font-barlow font-normal text-sm">$ 3.29</div>
+                <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
+              </div>
+            <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish10.png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className="mt-[16px] flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Healthy noodle with </div>
@@ -52,9 +112,9 @@ const Cards = () => {
                 <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">22 Bowls available</div>
               </div>
 
-              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/images(4).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish11.jpeg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Hot spicy fried rice  </div>
@@ -63,9 +123,9 @@ const Cards = () => {
                 <div className="mt-[4px] mb-[24px] flex justify-center text-[#ABBBC2] font-barlow font-normal text-sm">13 Bowls available</div>
               </div>
     
-              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[182px] h-[226px] flex flex-col items-center">
-                <div className="w-[182px] h-[226px] flex justify-center items-center -mt-[50px]">
-                  <img src="images/image(6).png" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
+              <div className=" bg-[#1F1D2B] rounded-[18px]  w-[200px] h-[226px] flex flex-col items-center">
+                <div className="w-[200px] h-[226px] flex justify-center items-center -mt-[50px]">
+                  <img src="images/hotdish12.jpg" alt="image" className="rounded-t-lg w-[132px] h-[132px] object-cover" />
                 </div>
               
                 <div className=" flex justify-center text-[#FFFFFF] font-barlow font-medium text-sm">Spicy instant noodle </div>
