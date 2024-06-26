1a42e3fc62492688ce0c33dcbcb025d67c948d0c
Your Name <your_email@example.com>
2024-06-26 23:21:51 +0500

    new.map

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -4,22 +4,34 @@ import React from 'react'
 
 const Cards = () => {
   const [cart, setCart] = useState([])
+  console.log("🚀 ~ Cards ~ cart:", cart)
   const newArray=[
     {id: 1, name: "hot and sour", quantity:12,},
     {id: 2, name: "hot and sour", quantity:13,},
     {id: 3, name: "hot and sour", quantity:14,}
   ]
- console.log(cart);
+
+function handleClick(item){
+  const filterData= cart.filter((obj)=>obj.id !== item.id) 
+  
+  setCart([...filterData, item])
+}
+
   return (
     <div>
-      <button onClick={() => setCart(...cart, )}>Add to cart</button>
-       <button onClick={() => setCart(...cart, )}>Add to cart</button>
-       <button onClick={() => setCart(...cart, )}>Add to cart</button>
+      {
+        newArray.map((item)=>(
+          <button onClick={()=>handleClick(item)}>Add to cart</button>
+
+        ))
+      }
+       
 
     </div>
   )
 }
 
 export default Cards
+
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <div className="flex">
-        <SideBar/>
+        {/* <SideBar/> */}
        <Cards/>
       </div>
     
