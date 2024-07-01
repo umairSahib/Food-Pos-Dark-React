f9dd3ef6dae9c13ca4f7d889a104e2b6ca0a2486
Your Name <your_email@example.com>
2024-07-01 18:00:15 +0500

    child to parent

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React from 'react'
 
-const Product = (props) => {
-  console.log("🚀 ~ Product ~ props:", props)
+const Product = ({ myData }) => {
+  const myObject = {
+    parentName: 'No Gym',
+    children: [
+      {
+        name: 'bands',
+        label: 'Bands'
+      },
+      {
+        name: 'jump-rope',
+        label: 'Jump Rope'
+      },
+      {
+        name: 'pvc-ripe-roller',
+        label: 'Pvc Ripe Roller'
+      },
+      {
+        name: 'foam-pad',
+        label: 'Foam pad'
+      },
+      {
+        name: 'mini-brands',
+        label: 'Mini Brands'
+      },
+      {
+        name: 'foam-roller',
+        label: 'Foam Roller'
+      },
+      {
+        name: 'sliders',
+        label: 'Sliders'
+      },
+      {
+        name: 'slant-board',
+        label: 'Slant Board'
+      },
+      {
+        name: 'dowel-rod',
+        label: 'Dowel Rod'
+      }
+    ]
+  }
 
-  
   return (
     <div>
-       {
-        props.equipment.map((item,index)=>(
-            <div > 
-            <div className='font-bold' key={index}>{item.parentName}</div>
-            {
-                item.children.map((item,index)=>(
-                    <div className='pl-4' key={index}>{item.label}</div>
-                ))
-            }
-            </div>
-           
-        ))
-       }
+      <button onClick={() => myData(myObject)}>Object</button>
     </div>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
